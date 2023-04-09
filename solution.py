64c7f265b1e818e0c899a3b9261e5e633da70d6e
D-Semenov <129876525+D-Semenov@users.noreply.github.com>
2023-04-09 21:15:16 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,10 @@ import numpy as np
 
 chat_id = 1474426447 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
+def solution(x) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    import scipy.stats as sts
+    alpha = 0.06 
+    t_value, p_value = sts.ttest_1samp(a = x, popmean = 300)
+    answer = p_value < alpha 
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return answer # Ваш ответ, True или False
